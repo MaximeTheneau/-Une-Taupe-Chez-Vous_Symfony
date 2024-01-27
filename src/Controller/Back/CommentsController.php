d5d198aaf04d7b134c3d05c86c0fd633dd625db2
Theneau Maxime <theneau.maxime@gmail.com>
2024-01-27 17:13:29 +0100

    Feat: Change comments email reply

--- src/Controller/Back/CommentsController.php
+++ src/Controller/Back/CommentsController.php
@@ -137,7 +137,7 @@ class CommentsController extends AbstractController
             $mailer->send($email);
 
             $emailReturn = (new TemplatedEmail())
-                ->to($_ENV['MAILER_TO_WEBMASTER'])
+                ->to($_ENV['MAILER_TO'])
                 ->from($_ENV['MAILER_TO'])
                 ->subject('Votre commentaire sur Une Taupe Chez Vous' )
                 ->htmlTemplate('emails/reply_notification_email_To.html.twig')
