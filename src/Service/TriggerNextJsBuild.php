76ecc7d1fd558cc5fd6d1a0619179b0a2c86b57e
Theneau Maxime <theneau.maxime@gmail.com>
2024-02-21 16:46:57 +0100

    Feat: Test build next

--- src/Service/TriggerNextJsBuild.php
+++ src/Service/TriggerNextJsBuild.php
@@ -10,15 +10,13 @@ class TriggerNextJsBuild
 
     public function triggerBuild(): array
     {
-        $nextJsPath = '/var/www/html/Une-Taupe-Chez-Vous_Next.js';
-
-        exec('cd ' . $nextJsPath . ' && npm run build 2>&1', $output, $return_var);
+        exec('npm run build-next  ../', $output, $return_var);
     
         if ($return_var !== 0) {
             $error = 'Une erreur est survenue lors de l\'exécution de la commande : ' . implode("\n", $output);
             return ['error' => $error];
         } else {
-            return ['success' => true];
+            return ['error' => null];
         }
 
     }
